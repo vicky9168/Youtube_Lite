a2845b8f0b39e71079a70dc39906f20653b3fe40
vicky9168 <dholevivek1111@gmail.com>
2023-05-20 18:13:43 +0530

    Initial Commit

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -23,7 +23,7 @@ const LeftNav = () => {
     }
   }
   return (
-   <div className={`md:block w-[240px] overflow-y-auto no-scrollbar h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${mobileMenu ? "translate-x-1" : ""}`} >
+   <div className={`md:block w-[240px] overflow-y-auto no-scrollbar h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${mobileMenu ? "translate-x-0" : ""}`} >
     <div className="flex px-5 flex-col" >
        {categories.map((item)=>{
         return (
